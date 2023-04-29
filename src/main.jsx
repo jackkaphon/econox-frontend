4dd6c817db2140d1628536fe6efb7006f35bf44d
jackkaphon <jackwchh@gmail.com>
2023-04-29 17:09:03 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import Index from './routes'
 import './index.css'
 
-window.$api = "http://localhost:3000"
+window.$api = "https://econox-backend.vercel.app"
+// window.$api = "http://localhost:3000"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
